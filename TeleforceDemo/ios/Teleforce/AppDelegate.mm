05163b2fee7a4b66764fd93cb3d3c3e877894428
zubair butt <zubyrbutt@gmail.com>
2023-03-24 06:23:17 +0500

    FCM ios fixes 2

--- TeleforceDemo/ios/Teleforce/AppDelegate.mm
+++ TeleforceDemo/ios/Teleforce/AppDelegate.mm
@@ -6,10 +6,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-[FIRApp configure];
   self.moduleName = @"Teleforce";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
+  [FIRApp configure];
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
